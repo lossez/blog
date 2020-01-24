3955801c4f7bc7211868bd17e7eb8871db0147ae
Rayha <rayha@pop-os.localdomain>
2020-01-24 21:24:01 +0700

    add responsive navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,22 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import logo from "../images/logo.png"
 
 const Header = () => {
-  return(
-  <header className = {
-      `w-full font-fredoka bg-white
-    items-center jutify-between border-b border-gray-200 sticky top-0 p-4             
-  `}>
-    <div className={`
-      flex container mx-auto flex-wrap xl:px-12
-    `}
-    >
-      <div className={`flex items-center flex-shrink-0 mr-6`}>
-          <Link to="/"> <img src = {logo} alt=""/></Link>  
+ const [isExpanded, toggleExpansion] = useState(false)
+
+  return (
+    <nav className="font-fredoka flex items-center justify-between flex-wrap bg-white p-4">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+         <Link to="/"> <img src = {logo} alt=""/></Link>  
       </div>
-      <div className={`
-        w-full block flex-grow lg:flex lg:items-center lg:w-auto
-      `}>
-        <div className={`text-sm lg:flex-grow`}></div>
+      <div className="block lg:hidden">
+        <button onClick={() => toggleExpansion(!isExpanded)} className="flex items-center px-4 py-3 bg-transparent border rounded text-black border-black">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className={`${ isExpanded ? `block` : `hidden` } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
+         <div className={`text-sm lg:flex-grow`}></div>
         <div>
           <Link to="/" className={`
             font-medium text-xl rounded-full block mt-4 lg:inline-block lg:mt-0 text-red-500 px-3 py-2 hover:bg-red-500 hover:text-white hover:font-extrabold focus:bg-red-500 focus:text-white focus:font-extrabold
@@ -34,8 +32,7 @@ const Header = () => {
           `}>Contact</Link>
         </div>
       </div>
-    </div>
-  </header>
+    </nav>
 )}
 
 export default Header
