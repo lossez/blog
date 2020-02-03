d044b384e8cc7e967ad2a2d107bcd4ef231c1396
Rayha <akatsuci99@gmail.com>
2020-02-03 17:08:08 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,14 @@
 import React from "react"
-import Layout from "../components/layout"
-import { Link, graphql } from 'gatsby'
 import SEO from "../components/seo"
-import Image from 'gatsby-image'
+
 
 class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const posts = data.allMarkdownRemark.edges
-
     return (
-      <Layout>
-        <SEO title="Home"/>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div className="inline-flex">
-
-            <article key = {
-              node.fields.slug
-            }
-            className = {
-              `max-w-full md:max-w-xs md:mr-2 lg:max-w-sm lg:mr-4 max-h-screen rounded shadow bg-white  mb-4`
-            }>
-               <Image fluid={node.frontmatter.thumbnail.childImageSharp.fluid}/>
-               <div className="px-6 py-4">
-                 <div className="font-bold text-xl ">
-                   <Link className={`text-black no-underline font-black`} to={node.fields.slug}>{title}</Link>
-                  </div>
-                  <div className="text-base text-gray-800 mb-2">
-                    <small>{node.frontmatter.date}</small>
-                  </div>
-                  <p className="text-gray-700 text-base" dangerouslySetInnerHTML={{__html: node.frontmatter.description || node.excerpt}}/>
-               </div>
-               <div className="mx-6 border-t-2 border-gray-200 -mt-6">
-              </div>
-               <div className="px-6 py-4">
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2">#Javascript</span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2">#Tutorial</span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 my-2">#nodejs</span>
-               </div>
-               <div className="mx-6 py-2 -mt-4">
-                  <Link className=" no-underline text-red-500 font-black">Continue Reading</Link>
-               </div>
-              
-            </article>  
-            </div>
-          )
-        })}
-      </Layout>
+     <div>
+      <SEO title="comming soon :)"/>
+        <p>COMMING SOON YA :)</p>
+     </div> 
     )
   }
 }
@@ -56,34 +16,3 @@ class IndexPage extends React.Component {
 
 export default IndexPage
 
-export const pageQuery = graphql `
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 600, maxHeight: 315) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
